d7459e40d278d27dbbaef637b9039defeecf16f5
Luce-22 <kihikalucy059@gmail.com>
2025-05-16 12:13:08 +0300

    Updated answers.sql

---
+++ answers.sql
@@ -0,0 +1,41 @@
+--Question 1
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(255)
+);
+
+INSERT INTO ProductDetail (OrderID, CustomerName, Products) 
+VALUES
+(101, 'John Doe', 'Laptop, Mouse'),
+(102, 'Jane Smith', 'Tablet, Keyboard, Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+--Question 2
+CREATE TABLE orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+INSERT INTO orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+CREATE TABLE product (
+    product_id INT PRIMARY KEY,
+    productName VARCHAR(100),
+    quantity INT,
+    order_id INT,
+    FOREIGN KEY (order_id) REFERENCES orders(OrderID)
+);
+
+INSERT INTO product (product_id, productName, quantity, order_id)
+VALUES 
+(1, 'Laptop', 2, 101),
+(2, 'Mouse', 1, 101),
+(3, 'Tablet', 3, 102),
+(4, 'Keyboard', 2, 102),
+(5, 'Mouse', 1, 102),
+(6, 'Phone', 1, 103);
